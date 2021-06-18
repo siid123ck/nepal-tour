e4aca4d6e44d1e17a9a18d1e3ee1101d98ed88b6
siid sharma <siid123ck@gmail.com>
2021-06-18 12:02:00 +1000

    changing data from file

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const Tour = require('../model/tour');
-const tours =JSON.parse(fs.readFileSync(`${__dirname}/../data/tours.json`, 'utf-8'))
 
 const checkTourId = (req, res, next, value) =>{
     
@@ -23,64 +22,76 @@ const getAllTours = async (req, res)=>{
   
  }
 
- const postTour = (req, res)=>{
-    const new_tour = new Tour(req.body)
-    testTour.save().then(data=>console.log(data)).catch(err=>console.log(`error: ${err}`))
+ const postTour = async (req, res)=>{
+    // const new_tour = new Tour(req.body);
+    // new_tour.save();
 
-    tours.push(new_tour);
-    fs.writeFile(`${__dirname}/data/tours.json`, JSON.stringify(tours), err=>{
+    try {
+        const new_tour = await Tour.create(req.body);
         res.status(201).json({
             status:'sucess',
             data:{new_tour}
         })
-
-    })
+    } catch (error) {
+        res.status(404).json({
+            status:'fail',
+            data:{error}
+        })
+    }
 }
 
-const getSingleTour =  (req, res)=>{
-    const tourId=req.params.id; 
-    console.log(req.params)
-   const tour = tours.find(item=>item._id===parseInt(tourId) )
-  console.log(`tourId: ${tourId}`)
-    if (!tour) {
-     return  res.status(404).json({
-           status:'fail', 
-           result:'not data found'
-       }) 
-    }
-    res.status(200).json({
-        status:'sucess',
-        result:tours.length,
-        data:{tour}
+const getSingleTour = async (req, res)=>{
+try {
+  const tour = await Tour.findById(req.params.id);
+  res.status(200).json({
+      status:'success',
+      data:{tour}
+  })
+} catch (err) {
+    res.status(404).json({
+        status:'fail',
+        data:{err}
     })
 }
+}
 
-const updateTour = (req, res)=>{
-    const tour = tours.find(item=>item._id===parseInt(req.params.id) )
-    if (!tour) {
-     return  res.status(404).json({
-           status:'fail', 
-           result:'not data found'
-       }) 
+const updateTour = async (req, res)=>{
+    try {
+        const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        })
+        console.log('tour' + tour)
+        console.log(req.body)
+        res.status(200).json({
+            status:'success',
+            data:{tour}
+        })
+
+    } catch (error) {
+        res.status(404).json({
+            status:'fail',
+            data:{error}
+        })
     }
-    res.status(200).json({
-        status:'sucess',
-        data:'data updated'
-    })
+ 
 };
 
-const deleteTour =  (req, res)=>{
-    const tour = tours.find(item=>item._id===parseInt(req.params.id) )
-    if (!tour) {
-     return  res.status(404).json({
-           status:'fail', 
-           result:'not data found'
-       }) 
+const deleteTour = async (req, res)=>{
+    try {
+        const tour = await Tour.findByIdAndDelete(req.params.id)
+        console.log('tour' + tour)
+        res.status(200).json({
+            status:'success',
+            data:{tour}
+        })
+
+    } catch (error) {
+        res.status(404).json({
+            status:'fail',
+            data:{error}
+        })
     }
-    res.status(204).json({
-        status:'sucess',
-        data:null
-    })
 }
 
 
--- model/tour.js
+++ model/tour.js
@@ -4,16 +4,36 @@ const tourSchema = new mongoose.Schema({
     name:{
         type:String, 
         required:[true, 'tour must have name'], 
-        unique:true
+        unique:true,
+        trim: true
     },
-    rating:{
-        type:String,  
-        required:true
-    },
+    ratingAverage: Number,
+    ratingQty:Number,
+    discount:Number,
     price:{
         type:String, 
         required:[true, 'tour must have price']
-    }
+    },
+    summary:{
+        type:String, 
+        required:[true, 'tour must have summary'],
+        trim:true
+    },
+    description:{
+        type:String, 
+        trim:true
+    },
+    imageCover:{
+        type:String, 
+        required:[true, 'tour must have imageCover'],
+        trim:true
+    },
+    images: [String],
+    createdAt:{
+        type:Date,
+        default:Date.now()
+    },
+    startDate:[Date]
 })
 const Tour = mongoose.model('Tour', tourSchema);
 
