953ca08356d17bbdc5c62e4806b7170e4796b2d2
siid sharma <siid123ck@gmail.com>
2021-09-26 02:15:31 +1000

    sending token via cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,7 +12,16 @@ const jsonToken = id=>jwt.sign({id}, process.env.JWT_SECRET, {
 }) 
 
 const sendResponseToken = (user, statusCode, res) =>{
-    const token = jsonToken(user._id)
+    const token = jsonToken(user._id); 
+    const cookieOptions = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE*24*60*60*1000),
+        http:true
+    }
+
+    if(process.env.NODE_ENV==='production') cookieOptions.secure = true;
+    
+    res.cookie('jwt', token, cookieOptions)
+    user.password=undefined;
     res.status(statusCode).json({
         status:'success',
         user,
